88db27f072329bd0141e4a433b35f7bf3fcc2608
Oliver Wang <wxh604328815@gmail.com>
2023-03-25 21:28:37 +1100

    first commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "resume" }
+)
+
+local options = { noremap = true }
+vim.keymap.set("i", "jk", "<Esc>", options)
