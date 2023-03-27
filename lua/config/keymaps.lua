cad4676497683b68c18767499de9d3b1dd3f6f6b
Oliver Wang <wxh604328815@gmail.com>
2023-03-27 11:29:19 +1100

    temp save

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
 vim.keymap.set(
   "n",
   "<leader>sx",
@@ -10,3 +22,9 @@ vim.keymap.set(
 
 local options = { noremap = true }
 vim.keymap.set("i", "jk", "<Esc>", options)
+vim.keymap.set("n", "U", "<C-r>")
+-- vim.keymap.set("n", "L", "$")
+-- vim.keymap.set("n", "H", "^")
+
+-- map("n", "<S-j>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+-- map("n", "<S-k>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_floating_blur = 0
+  vim.g.neovide_floating_opacity = 90
+end
